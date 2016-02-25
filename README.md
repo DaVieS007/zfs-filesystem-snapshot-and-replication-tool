218903452e65d50b43345f3e394bcf629170a34a
DaVieS / Hlavaji Viktor <davies@npulse.net>
2016-02-25 03:39:31 +0100

    Initial Release

---
+++ README.md
@@ -0,0 +1,7 @@
+Utilities for ZFS Auto Incremental snapshot and replication.
+Author: Hlavaji Viktor / DaVieS
+License: BSD License
+
+Status: RELIABLE / TESTED
+Usage: MANUAL / CRON
+Language: PHP
---
+++ ZFS-Local-Pool-Sync/zfsrep.php
@@ -0,0 +1,566 @@
+<?php
+
+/**
+    Author: Hlavaji Viktor / DaVieS
+	    davies@npulse.net / nPulse.net
+    License: BSD License
+
+    Semi Auto PHP ZFS Incremental Synchronisation Tool between local pools
+**/
+
+$config = array();
+$config["snapshot"] = false; // DO SNAPSHOTING
+//$config["snapshot_datasets"][] = "zroot";
+$config["snapshot_datasets"][] = "zroot/storage/vm/datalogger";
+$config["snapshot_datasets"][] = "zroot/storage/vm/fileserver";
+$config["snapshot_datasets"][] = "zroot/storage/vm/mn";
+$config["snapshot_datasets"][] = "zroot/storage/vm/old_sys";
+$config["snapshot_datasets"][] = "zroot/storage/vm/openerp";
+//$config["snapshot_datasets"][] = "zroot/usr";
+//$config["snapshot_datasets"][] = "zroot/usr/ports";
+//$config["snapshot_datasets"][] = "zroot/var";
+
+$config["keep_snapshots"] = 14; //MAXIMUM SNAPSHOTS
+$config["snapshot_interval"] = 60*24; //minute //SNAPSHOT INTERVAL
+
+$config["sync"] = true; // DO SYNCING
+$config["sync_datasets"][] = "zroot/devel:zbackup/devel";
+$config["sync_datasets"][] = "zroot/ext-images:zbackup/ext-images";
+$config["sync_datasets"][] = "zroot/documents:zbackup/documents";
+$config["sync_datasets"][] = "zroot/backup:zbackup/backup";
+$config["sync_datasets"][] = "zroot/photoshop:zbackup/photoshop";
+$config["sync_interval"] = 0; //minute //SYNC INTERVAL
+
+$config["version"] = 1;
+
+$config["mutex_file"] = "/zroot/zfsrep.lock";
+$config["data_file"] = "/zroot/zfsrep.data";
+$config["log_file"] = "/zroot/zfsrep.log";
+
+/** LOG **/
+function logger($msg)
+{
+	global $config;
+	if($msg)
+	{
+		$data = "[".date("Y-m-d H:i:s")."] ".$msg."\n";
+		echo($data);
+		
+		$fp = fopen($config["log_file"], 'a');
+		fwrite($fp, $data);
+		fclose($fp);	
+	}
+}
+/** LOG **/
+
+/** CLASS STORE_DATA **/
+class store_data
+{
+	var $data;
+	var $file;
+	
+	/** CONSTRUCTOR **/
+	function store_data($file)
+	{
+		$this->data = array();
+		$this->file = $file;
+	}
+	/** CONSTRUCTOR **/
+	
+	/** WRITE **/
+	function write()
+	{	
+		$temp = serialize($this->data);
+		$fp = fopen($this->file,"w");
+		if(!$fp)
+		{
+			logger("Unable to write config");
+			return false;
+		}
+		else
+		{
+			fwrite($fp,$temp);
+			fclose($fp);
+		}
+	}
+	/** WRITE **/
+
+	/** READ **/
+	function read()
+	{
+		$temp = file_get_contents($this->file);
+		if(!$temp)
+		{
+			logger("Unable to read config");
+			return false;
+		}
+		else
+		{
+			$this->data = unserialize($temp);
+		}
+	}
+	/** READ **/
+
+}
+/** CLASS STORE_DATA **/
+
+/** CLASS MUTEXER **/
+class mutexer
+{
+	var $locked;
+	var $fp;
+	var $file;
+	
+	/** CONSTRUCTOR **/
+	function mutexer($file)
+	{
+		$locked = false;
+		$this->file = $file;
+	}
+	/** CONSTRUCTOR **/
+
+	/** LOCK **/
+	function lock()
+	{		
+		$this->fp = fopen($this->file,"w");
+		if(!$this->fp)
+		{
+			logger("Unable to open file: ".$this->file);
+			return false;
+		}
+		else
+		{
+			if(!flock($this->fp,LOCK_EX))
+			{
+				logger("Unable to lock file: ".$this->file);
+				return false;
+			}
+			else
+			{
+				$this->locked = true;
+				logger("Mutex Locked");
+				return true;
+			}
+		}
+	}
+	/** LOCK **/
+
+
+	/** UNLOCK **/
+	function unlock()
+	{
+		if(!$this->fp || !$this->locked)
+		{
+			logger("Unable to unlock, beacuse no yet locked");
+			return false;
+		}
+		
+		if(!flock($this->fp,LOCK_UN))
+		{
+			logger("Unable to unlock file: ".$this->file);
+			return false;
+		}
+		fclose($this->fp);
+		logger("Mutex UnLocked");
+		return true;
+	}
+	/** UNLOCK **/
+
+	/** CHECK **/
+	function check()
+	{		
+		if($this->locked)
+		{
+			return false;
+		}
+		else
+		{
+			$fp = fopen($this->file,"w");
+			if(!$fp)
+			{
+				logger("Unable to open file: ".$this->file);
+				return false;
+			}
+			else
+			{
+				if(!flock($fp,LOCK_EX | LOCK_NB))
+				{
+					fclose($fp);
+					return false;
+				}
+				else
+				{
+					if(!flock($fp,LOCK_UN))
+					{
+						logger("Unable to unlock file: ".$this->file);
+						fclose($fp);
+						return false;
+					}
+					else
+					{
+						fclose($fp);
+						return true;
+					}
+				}
+			}
+		}
+	}
+	/** CHECK **/
+
+}
+/** CLASS MUTEXER **/
+
+/** CLASS ZFS_HANDLER **/
+class zfs_handler
+{
+var $data;
+
+	/** CONSTRUCTOR **/
+	function zfs_handler($data)
+	{
+		$this->data = $data;
+	}
+	/** CONSTRUCTOR **/
+
+	/** SYNC **/
+	function sync($src,$dst,$server)
+	{
+		$arr = $this->snapshot_list($src,"bck");
+		if(count($arr))
+		{
+			logger("Syncing already running, so update incrementaly ..");
+			$ret = $this->snapshot_create($src,"bck");
+			if(!$ret)
+			{
+				logger("ERROR COULD NOT SYNC");
+				return false;
+			}
+			$sname = $ret;
+			$sname2 = $this->data->data["syncs"][$src]["last_success"];
+			
+			if($this->inc_copy($sname2,$sname,$dst,$server))
+			{
+				$this->data->data["syncs"][$src]["last_success"] = $sname;
+				$this->data->write();
+				$this->snapshot_delete($sname2);
+				return true;
+			}
+			else
+			{
+				$this->snapshot_delete($sname);
+				return false;
+			}
+			
+		}
+		else
+		{
+			logger("No sync snapshots, have to do full sync");
+			$ret = $this->snapshot_create($src,"bck");
+			if(!$ret)
+			{
+				logger("ERROR COULD NOT SYNC");
+				return false;
+			}
+			
+			$sname = $ret;
+
+			if($this->copy($sname,$dst,$server))
+			{
+				$this->data->data["syncs"][$src]["last_success"] = $sname;
+				$this->data->write();				
+				return true;
+			}
+			else
+			{
+				$this->snapshot_delete($sname);
+				return false;
+			}
+		}
+	}
+	/** SYNC **/
+
+	/** COPY **/
+	function copy($src,$dst,$server)
+	{
+		global $config;
+		$temp = shell_exec("zfs send \"".$src."\" | zfs recv -vFd ".$dst." 2>&1");
+		if(strstr($temp,"received"))
+		{
+			logger("SUCCESS: ".$temp);
+			$temp = str_replace("\r","",$temp);
+			$temp = explode("\n",$temp);
+			$temp = explode(" ",$temp[0]);
+			$temp = $temp[6];
+			
+			$name = explode("@",$temp);
+
+			logger("Rename on DST: ".$temp." to: ".$name[0]."@latest_bck");
+			logger(shell_exec("zfs rename ".$temp." ".$name[0]."@latest_bck 2>&1"));
+			return true;
+		}
+		else
+		{
+			logger("ERROR: ".$temp);
+			return false;
+		}
+	}
+	/** COPY **/
+
+	/** INC_COPY **/
+	function inc_copy($src,$src2,$dst,$server)
+	{
+	global $config;
+		$temp = shell_exec("zfs send -i \"".$src."\" \"".$src2."\" | zfs recv -vFd ".$dst." 2>&1");
+		if(strstr($temp,"received"))
+		{
+			logger("SUCCESS: ".$temp);
+			
+			$temp = str_replace("\r","",$temp);
+			$temp = explode("\n",$temp);
+			$temp = explode(" ",$temp[0]);
+			$temp = $temp[6];
+			
+			
+			$name = explode("@",$temp);
+			logger("Delete on DST: ".$name[0]."@latest_bck");
+			
+			logger(shell_exec("zfs destroy ".$name[0]."@latest_bck 2>&1"));
+
+			logger("Rename on DST: ".$temp." to: ".$name[0]."@latest_bck");
+			logger(shell_exec("zfs rename ".$temp." ".$name[0]."@latest_bck 2>&1"));
+			
+			return true;
+		}
+		else
+		{
+			logger("ERROR: ".$temp);
+			return false;
+		}
+		return true;
+	}
+	/** INC_COPY **/
+
+	
+	/** SNAPSHOT_CLEANUP **/
+	function snapshot_cleanup($name, $num)
+	{
+		if(!$this->check_dataset($name))
+		{
+			logger("Failed to cleanup snapshots because dataset: ".$name." not exists!");
+			return false;
+		}
+
+		$arr = $this->snapshot_list($name,"snap");
+		
+		$i = count($arr);
+		while($i > $num)
+		{
+			ksort($arr);
+			reset($arr);
+			list($key,$val) = each($arr);
+			$this->snapshot_delete($val);
+			unset($arr[$key]);
+			
+			$i = count($arr);
+		}
+
+	}
+	/** SNAPSHOT CLEANUP **/	
+	
+	/** SNAPSHOT_DELETE **/
+	function snapshot_delete($name)
+	{
+		if(!strstr($name,"@"))
+		{
+			logger("Could not delete snapshot: ".$name.", because not a snapshot");
+			return false;
+		}
+		
+		$temp = shell_exec("zfs destroy \"".$name."\" 2>&1");
+		if(strstr($temp,"could"))
+		{
+			logger("ERROR: ".$temp);
+			return false;
+		}
+		else
+		{
+			logger("Snapshot deleted: ".$name);
+			return true;
+		}
+	}
+	/** SNAPSHOT_DELETE **/
+
+	/** SNAPSHOT_CREATE **/
+	function snapshot_create($name,$prefix = "snap")
+	{
+		if(!$this->check_dataset($name))
+		{
+			logger("Failed to create snapshot because dataset: ".$name." not exists!");
+			return false;
+		}
+	
+		if(!isset($this->data->data["snapshots"][$name]["index"]))
+		{
+			$this->data->data["snapshots"][$name]["index"] = 0;
+		}
+		
+		$sname = $name."@".$prefix.".N.".$this->data->data["snapshots"][$name]["index"].".N.".date("Y-m-d_H:i:s",time());
+		
+		$temp = shell_exec("zfs snapshot \"".$sname."\" 2>&1");
+		if(strstr($temp,"cannot"))
+		{
+			logger("ERROR: ".$temp);
+			return false;
+		}
+		else
+		{
+			logger("Snapshot Created: ".$sname);
+			$this->data->data["snapshots"][$name]["index"]++;
+			$this->data->write();		
+			return $sname;
+		}
+	}
+	/** SNAPSHOT CREATE **/
+
+	/** CHECK_DATASET **/
+	function check_dataset($name)
+	{
+		$temp = shell_exec("zfs list");
+		$temp = explode("\n",$temp);
+		while(list($key,$val) = each($temp))
+		{
+			$val = explode(" ",$val);
+			if($val[0] == $name)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	/** CHECK_DATASET **/
+	
+	/** SNAPSHOT_LIST **/
+	function snapshot_list($filter,$filter2)
+	{
+		$ret = array();
+		$temp = shell_exec("zfs list -t snapshot");
+		$temp = explode("\n",$temp);
+		
+		$i = 0;
+		while(list($key,$val) = each($temp))
+		{
+			if(!$i)
+			{
+				$i++;
+				continue;
+			}
+			
+			$val = explode(" ",$val);
+			$name = explode("@",$val[0]);
+		
+			if(!isset($name[1]))
+			{
+				continue;
+			}
+			
+			$data = explode(".N.",$name[1]);
+			if($data[0] != $filter2)
+			{
+				continue;
+			}
+			
+
+			if($name[0] == $filter)
+			{
+				$ret[$data[1]] = $val[0];
+			}
+		}
+		return $ret;
+	}
+	/** SNAPSHOT_LIST **/
+
+}
+/** CLASS ZFS_HANDLER **/
+
+$mutex = new mutexer($config["mutex_file"]);
+$data = new store_data($config["data_file"]);
+$zfs = new zfs_handler($data);
+
+if($mutex->check())
+{
+	$mutex->lock();
+	$data->read();
+	/* INITIALISE */
+	if($data->data["version"] < $config["version"])
+	{
+		logger("Have to inititalise system");
+		$data->data = array();
+		$data->data["version"] = $config["version"];
+		$data->write();
+	}
+	else if($data->data["version"] > $config["version"])
+	{
+		logger("Program version is old");
+		die();
+	}
+
+
+	/** SNAPSHOTS **/
+	if($config["snapshot"] == true)
+	{
+		if(($data->data["last_snapshot"] + ($config["snapshot_interval"] * 60)) < time())
+		{
+			logger("Snapshoting ..");
+			$data->data["last_snapshot"] = time();
+			$data->write();
+			
+			while(list($key,$val) = each($config["snapshot_datasets"]))
+			{
+				$zfs->snapshot_create($val,"snap");
+				$zfs->snapshot_cleanup($val,$config["keep_snapshots"]);
+			}			
+			
+		}
+	}
+	/** SNAPSHOTS **/
+
+	
+	/** SYNC **/
+	if($config["sync"] == true)
+	{
+	
+		if(($data->data["last_sync"] + ($config["sync_interval"] * 60)) < time())
+		{
+			logger("Syncing ..");
+			$data->data["last_sync"] = time();
+			$data->write();
+
+			while(list($key,$val) = each($config["sync_datasets"]))
+			{
+				$val = explode(":",$val);
+				if($val[0] && $val[1])
+				{
+					$zfs->sync($val[0],$val[1],"");
+				}
+				else
+				{
+					logger("Invalid SYNC datasets");
+				}
+			}			
+
+		}
+	}
+		
+	/** SYNC **/
+
+	
+	$mutex->unlock();
+}
+else
+{
+	
+}
+
+logger("Process Ended\n\n");
+
+?>
